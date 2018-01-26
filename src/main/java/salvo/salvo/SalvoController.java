e2ce076ef4d44dca58640bf50da12752eac71e92
Gustavo <gustavosamo@gmail.com>
2018-01-26 20:31:48 +0100

    Salvo, sacar id's de Games 26/01 20:31h

--- src/main/java/salvo/salvo/SalvoController.java
+++ src/main/java/salvo/salvo/SalvoController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -14,9 +15,15 @@ public class SalvoController {
     private GameRepository repoGames;
 
     @RequestMapping("api/games")
-    public List<Game> getGames() {
+    public List<Long> metodoCogerGamesIds() {
 //        System.out.println("Repogames"+repoGames.findAll());
-        return repoGames.findAll();
+        ArrayList<Long> gamesIds = new ArrayList<>();
+        for(int i = 0; i<repoGames.findAll().size(); i++){
+            gamesIds.add(repoGames.findAll().get(i).getId());
+
+        }
+
+        return gamesIds;
 
     }
 }
