f484f264e572c748234ff7ee712cdf6f8b0c2dd1
Gustavo <gustavosamo@gmail.com>
2018-01-30 14:19:38 +0100

    Salvo, task 2.7 30/01 14:19h Bruno explica stream.map

--- src/main/resources/static/web/futbolMobile.js
+++ src/main/resources/static/web/futbolMobile.js
@@ -9,10 +9,25 @@ function crearFutbolMobile(data) {
 
 console.log(data);
 
-//    var contenidoFutbolMobile = '<p>' + data.equipo["0"].nombre + '</p>';
+    var contenidoFutbolMobile = '<ul>PARTIDA'+
+        '<li>' + 'Creado: '+ data["0"].Created + '</li>'+
+        '<li>'+'ID: '+data["0"].ID+'<li>'+
+        '<ul>GAMEPLAYER'+
+        '<li>ID: '+data["0"].gamePlayers["0"].id+'</li>'+
+        '<ul>JUGADOR'+
+        '<li>MAIL: '+data["0"].gamePlayers["0"].players.email+'</li>'+
+        '<li>ID: '+data["0"].gamePlayers["0"].players.id+'</li>'+
+        '</ul>'+'</ul>'+
+        '<ul>GAMEPLAYER'+
+        '<li>ID: '+data["0"].gamePlayers["1"].id+'</li>'+
+        '<ul>JUGADOR'+
+        '<li>MAIL: '+data["0"].gamePlayers["1"].players.email+'</li>'+
+        '<li>ID: '+data["0"].gamePlayers["1"].players.id+'</li>'
+        '</ul>'+'</ul>'
+        ;
 
-//    console.log(contenidoFutbolMobile);
+    console.log(contenidoFutbolMobile);
 
-//    document.getElementById("futbolMobileID").innerHTML = contenidoFutbolMobile;
+    document.getElementById("futbolMobileID").innerHTML = contenidoFutbolMobile;
 
 }
