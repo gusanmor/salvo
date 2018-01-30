c5eef3f3805806ed9e15e66301492791b2da8ae1
Gustavo <gustavosamo@gmail.com>
2018-01-30 13:11:23 +0100

    Salvo, task 2.7 30/01 13:10h Bruno explica stream.map

--- src/main/java/salvo/salvo/SalvoController.java
+++ src/main/java/salvo/salvo/SalvoController.java
@@ -17,21 +17,39 @@ public class SalvoController {
     @RequestMapping("api/games")
     public ArrayList IDyCreatedMetodo(Game game) {
         ArrayList IDyCreatedList = new ArrayList();
+        List<Game> games = repoGames.findAll();
 
         Set<GamePlayer> gamePlayers = game.getGamePlayers();
 
-        for(int i = 0; i<repoGames.findAll().size(); i++){
+        for(int i = 0; i<games.size(); i++){
             Map<String, Object> IDyCreatedMap = new HashMap<String, Object>();
-            IDyCreatedMap.put("ID", repoGames.findAll().get(i).getId());
-            IDyCreatedMap.put("Created", repoGames.findAll().get(i).getFechaVar());
-            IDyCreatedMap.put("gamePlayers", repoGames.findAll().get(i).getGamePlayers().stream()
-                    .map(gameNoEntiendo -> gamePlayerMetodo(gameNoEntiendo))
-                    .collect(Collectors.toList()));
+            IDyCreatedMap.put("ID", games.get(i).getId());
+            IDyCreatedMap.put("Created", games.get(i).getFechaVar());
+
+            Set<GamePlayer> input = games.get(i).getGamePlayers();
+            List<Map<String, Object>> output = new ArrayList<>();
+
+            // Option 1
+            for (GamePlayer gameNoEntiendo : input) {
+                Map<String, Object> gpDTO = gamePlayerMetodo(gameNoEntiendo);
+                output.add(gpDTO);
+            }
+
+            // Option 2
+//            output = input.stream()
+//                    .map(gameNoEntiendo -> gamePlayerMetodo(gameNoEntiendo))
+//                    .collect(Collectors.toList());
+
+
+//            IDyCreatedMap.put("gamePlayers", games.get(i).getGamePlayers().stream()
+//                    .map(gameNoEntiendo -> gamePlayerMetodo(gameNoEntiendo))
+//                    .collect(Collectors.toList()));
+
+            IDyCreatedMap.put("gamePlayers", output);
+
 
             IDyCreatedList.add(IDyCreatedMap);
         }
-
-
         return IDyCreatedList;
 
 //        ArrayList IDsGamesArr = new ArrayList<>();
