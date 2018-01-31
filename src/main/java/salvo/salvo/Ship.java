c4cf53d3c0ad2b8c125bb4d060a244c8f29a4acb
Gustavo <gustavosamo@gmail.com>
2018-01-31 13:17:45 +0100

    Salvo, task 3 31/01 13:17h

--- src/main/java/salvo/salvo/Ship.java
+++ src/main/java/salvo/salvo/Ship.java
@@ -1,7 +1,32 @@
 package salvo.salvo;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Ship {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+
+    private String tipoBarco;
+
+    public Ship() { }
+
+    public Ship(String tipoBarcoPar) {
+        tipoBarco = tipoBarcoPar;
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTipoBarco() {
+        return tipoBarco;
+    }
+
 }
