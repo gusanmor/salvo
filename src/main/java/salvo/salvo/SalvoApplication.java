23d4df973b240ab80f73bdadeab21f2b53461d48
Gustavo <gustavosamo@gmail.com>
2018-02-02 10:15:52 +0100

    Salvo, task 3 02/02 10:15h

--- src/main/java/salvo/salvo/SalvoApplication.java
+++ src/main/java/salvo/salvo/SalvoApplication.java
@@ -17,7 +17,6 @@ public class SalvoApplication {
         SpringApplication.run(SalvoApplication.class, args);
     }
 
-
     @Bean
     public CommandLineRunner initData(PlayerRepository playerRepository,
                                       GameRepository gameRepository,
@@ -30,13 +29,6 @@ public class SalvoApplication {
             Player jugador2Var = new Player("c.obrian@ctu.gov");
             Player jugador3Var = new Player("kim_bauer@gmail.com");
             Player jugador4Var = new Player("t.almeida@ctu.gov");
-
-
-            playerRepository.save(jugador1Var);
-            playerRepository.save(jugador2Var);
-            playerRepository.save(jugador3Var);
-            playerRepository.save(jugador4Var);
-
 
             Date fecha1Var = new Date();
             Date fecha2Var = Date.from(fecha1Var.toInstant().plusSeconds(3600));
@@ -52,13 +44,6 @@ public class SalvoApplication {
             Game partida5Var = new Game(fecha5Var);
             Game partida6Var = new Game(fecha6Var);
 
-            gameRepository.save(partida1Var);
-            gameRepository.save(partida2Var);
-            gameRepository.save(partida3Var);
-//            gameRepository.save(partida4Var);
-//            gameRepository.save(partida5Var);
-//            gameRepository.save(partida6Var);
-
             GamePlayer gamePlayer1Var = new GamePlayer(jugador1Var, partida1Var);
             GamePlayer gamePlayer2Var = new GamePlayer(jugador2Var, partida1Var);
             GamePlayer gamePlayer3Var = new GamePlayer(jugador1Var, partida2Var);
@@ -66,19 +51,28 @@ public class SalvoApplication {
             GamePlayer gamePlayer5Var = new GamePlayer(jugador2Var, partida3Var);
             GamePlayer gamePlayer6Var = new GamePlayer(jugador4Var, partida3Var);
 
-            gamePlayerRepository.save(gamePlayer1Var);
-            gamePlayerRepository.save(gamePlayer2Var);
-            gamePlayerRepository.save(gamePlayer3Var);
-            gamePlayerRepository.save(gamePlayer4Var);
-            gamePlayerRepository.save(gamePlayer5Var);
-            gamePlayerRepository.save(gamePlayer6Var);
-
 
             ArrayList<String> localBarco1 = new ArrayList<String>(Arrays.asList("A1", "A2", "A3"));
             ArrayList<String> localBarco2 = new ArrayList<String>(Arrays.asList("E1", "F1", "G1"));
 
             Ship ship1 = new Ship("Destroyer", localBarco1);
             Ship ship2 = new Ship("Submarine", localBarco2);
+
+            playerRepository.save(jugador1Var);
+            playerRepository.save(jugador2Var);
+            playerRepository.save(jugador3Var);
+            playerRepository.save(jugador4Var);
+
+            gameRepository.save(partida1Var);
+            gameRepository.save(partida2Var);
+            gameRepository.save(partida3Var);
+
+            gamePlayerRepository.save(gamePlayer1Var);
+            gamePlayerRepository.save(gamePlayer2Var);
+            gamePlayerRepository.save(gamePlayer3Var);
+            gamePlayerRepository.save(gamePlayer4Var);
+            gamePlayerRepository.save(gamePlayer5Var);
+            gamePlayerRepository.save(gamePlayer6Var);
 
             shipRepository.save(ship1);
             shipRepository.save(ship2);
