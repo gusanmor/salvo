fd46a5eb248b142f5a7ab4c4137191ca678add15
Gustavo <gustavosamo@gmail.com>
2018-02-05 14:32:17 +0100

    crear tabla, creando 2 arrays y 2 fors

--- src/main/resources/static/web/game.js
+++ src/main/resources/static/web/game.js
@@ -1,4 +1,4 @@
-$.getJSON("http://localhost:8080/api/games", function (data) {
+$.getJSON("http://localhost:8080/api/game_view/1", function (data) {
 
     	console.log(data);
     crearFutbolMobile(data);
@@ -9,13 +9,32 @@ function crearFutbolMobile(data) {
 
 // console.log(data);
     
-    var contenidoRejillaBarcos1 = {};
+    var contenidoRejillaBarcos1 = "";
     
     // for (var i=0; i<data.length; i++) {
     // var fecha2 = new Date(data[i].Created);
     // var fecha3 = fecha2.toString();
 
-        contenidoRejillaBarcos1 = '<div>hola2</div>';
+    arrayNumerosTabla = ["0", "1","2","3","4","5","6","7","8","9","10"];
+    console.log(arrayNumerosTabla);
+
+    arrayLetrasTabla = ["A", "B","C","D","F","G","H","I","J"];
+
+    for (var i=0; i<arrayNumerosTabla.length; i++) {
+
+        contenidoRejillaBarcos1 += '<tr>'+
+        '<td>'+arrayNumerosTabla[i]+'</td>';
+
+    }
+
+    for (var i=0; i<arrayNumerosTabla.length; i++) {
+
+        contenidoRejillaBarcos1 += '<tr>'+
+            '<td>'+arrayLetrasTabla[0]+'</td>';
+
+    }
+
+        // contenidoRejillaBarcos1 = '<tr><td>0<td>DOS</td></tr>';
 
         
     // }
