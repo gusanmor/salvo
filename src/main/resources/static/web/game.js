e740daad8132b1182b31765d3361a5a9659c912b
Gustavo <gustavosamo@gmail.com>
2018-02-05 11:56:43 +0100

    empiezo front end de la rejilla de barcos4

--- src/main/resources/static/web/game.js
+++ src/main/resources/static/web/game.js
@@ -1,42 +1,27 @@
 $.getJSON("http://localhost:8080/api/games", function (data) {
 
     	console.log(data);
-    // crearFutbolMobile(data);
+    crearFutbolMobile(data);
 
 });
 
 function crearFutbolMobile(data) {
 
-console.log(data);
+// console.log(data);
     
-    var contenidoFutbolMobile = {};
+    var contenidoRejillaBarcos1 = {};
     
-    for (var i=0; i<data.length; i++) {
-    var fecha2 = new Date(data[i].Created);
-    var fecha3 = fecha2.toString();
-    
-    contenidoFutbolMobile += '<div>_</div>'+
-        '<div class="fotarIzquierda">'+
-        '<ul>PARTIDA'+
-        '<li>'+'ID: '+data[i].ID+'</li>'+
-        '<li>' + 'Creado: '+ fecha3 + '</li>'+
-        '<ul>GAMEPLAYER'+
-        '<li>ID: '+data[i].gamePlayers["0"].id+'</li>'+
-        '<div>JUGADOR</div>'+
-        '<li>ID: '+data[i].gamePlayers["0"].players.id+'</li>'+
-        '<li>MAIL: '+data[i].gamePlayers["0"].players.email+'</li>'+
-        '</ul>'+
-        '<ul>GAMEPLAYER'+
-        '<li>ID: '+data[i].gamePlayers["1"].id+'</li>'+
-        '<div>JUGADOR</div>'+
-        '<li>ID: '+data[i].gamePlayers["1"].players.id+'</li>'+
-        '<li>MAIL: '+data[i].gamePlayers["1"].players.email+'</li>'+
-        '</ul>'+'</div>';
+    // for (var i=0; i<data.length; i++) {
+    // var fecha2 = new Date(data[i].Created);
+    // var fecha3 = fecha2.toString();
+
+        contenidoRejillaBarcos1 = '<div>hola2</div>';
+
         
-    }
+    // }
 
 //    console.log(contenidoFutbolMobile);
 
-    document.getElementById("gameID").innerHTML = contenidoFutbolMobile;
+    document.getElementById("gameID").innerHTML = contenidoRejillaBarcos1;
 
 }
