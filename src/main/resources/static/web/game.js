433b44e1232fa5d3403564b3a1a13ae62475f971
Gustavo <gustavosamo@gmail.com>
2018-02-09 12:37:59 +0100

    task 4 terminada

--- src/main/resources/static/web/game.js
+++ src/main/resources/static/web/game.js
@@ -1,6 +1,7 @@
 $.getJSON("http://localhost:8080/api/game_view/"+limpiarURL(document.location.search), function (data) {
     console.log(data);
-    crearRejillaBarcos(data);
+    crearRejiBarcosYsalvos(data);
+    crearJugadoresGV(data);
 });
 
 function limpiarURL(search) {
@@ -15,12 +16,12 @@ function limpiarURL(search) {
     return obj2;
 }
 
-function crearRejillaBarcos(data) {
-    
-    var contenidoRejillaBarcos1 = "";
-    arrayNumerosTabla = [" ", "1","2","3","4","5","6","7","8","9","10"];
+function crearRejiBarcosYsalvos(data) {
 
-    arrayLetrasTabla = ["A", "B","C","D","E","F","G","H","I","J"];
+    var contenidoRejillaBarcos1 = "";
+    arrayNumerosTabla = [" ", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+
+    arrayLetrasTabla = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 
     contenidoRejillaBarcos1 += "<tr>";
 
@@ -32,33 +33,33 @@ function crearRejillaBarcos(data) {
         keyContrario = 0;
     }
 
-    for (var i=0; i<arrayNumerosTabla.length; i++) {
+    for (var i = 0; i < arrayNumerosTabla.length; i++) {
 
-        contenidoRejillaBarcos1 += '<td>'+arrayNumerosTabla[i]+'</td>';
+        contenidoRejillaBarcos1 += '<td>' + arrayNumerosTabla[i] + '</td>';
     }
 
     contenidoRejillaBarcos1 += "</tr><tr>";
     contenidoRejillaSalvos1 = contenidoRejillaBarcos1;
 
-    for (var j=0; j<arrayLetrasTabla.length; j++) {
+    for (var j = 0; j < arrayLetrasTabla.length; j++) {
 
-        contenidoRejillaBarcos1 += '<td>'+arrayLetrasTabla[j]+'</td>';
-        contenidoRejillaSalvos1 += '<td>'+arrayLetrasTabla[j]+'</td>';
+        contenidoRejillaBarcos1 += '<td>' + arrayLetrasTabla[j] + '</td>';
+        contenidoRejillaSalvos1 += '<td>' + arrayLetrasTabla[j] + '</td>';
 
-        for (var k=1; k<arrayNumerosTabla.length; k++) {
+        for (var k = 1; k < arrayNumerosTabla.length; k++) {
             var claseBarco = "celdaSinBarco";
             var claseSalvo = "celdaSinSalvo";
-            var idCeldaBarco = arrayLetrasTabla[j]+arrayNumerosTabla[k];
+            var idCeldaBarco = arrayLetrasTabla[j] + arrayNumerosTabla[k];
             var idCeldaSalvo = idCeldaBarco;
 
             // --------PINTAR CELDAS CON BARCOS-----------
-            for (var l=0; l<data.ships.length; l++) {
-                for (var m=0; m<data.ships[l].locations.length; m++) {
-                    if (data.ships[l].locations[m]==idCeldaBarco){
+            for (var l = 0; l < data.ships.length; l++) {
+                for (var m = 0; m < data.ships[l].locations.length; m++) {
+                    if (data.ships[l].locations[m] == idCeldaBarco) {
                         var claseBarco = "celdaBarco";
-                        for (var p=0; p<data.salvoes[keyContrario].locations.length; p++) {
-                            for (var q=0; q<data.salvoes[keyContrario].locations[p].length; q++) {
-                                if (data.salvoes[keyContrario].locations[p][q]==idCeldaBarco){
+                        for (var p = 0; p < data.salvoes[keyContrario].locations.length; p++) {
+                            for (var q = 0; q < data.salvoes[keyContrario].locations[p].length; q++) {
+                                if (data.salvoes[keyContrario].locations[p][q] == idCeldaBarco) {
                                     var claseBarco = "celdaTocado";
                                     idCeldaBarco = data.salvoes[keyContrario].turn[p];
                                 }
@@ -70,19 +71,19 @@ function crearRejillaBarcos(data) {
 
             // -------------PINTAR CELDAS CON SALVOS------------
 
-            for (var n=0; n<data.salvoes[keyTuJugador].locations.length; n++) {
+            for (var n = 0; n < data.salvoes[keyTuJugador].locations.length; n++) {
                 // console.log(data.salvoes["0"].locations["0"].locations[n]);
-                for (var o=0; o<data.salvoes[keyTuJugador].locations[n].length; o++) {
+                for (var o = 0; o < data.salvoes[keyTuJugador].locations[n].length; o++) {
                     // console.log(data.salvoes["0"].locations[n][o]);
-                    if (data.salvoes[keyTuJugador].locations[n][o]==idCeldaBarco){
+                    if (data.salvoes[keyTuJugador].locations[n][o] == idCeldaBarco) {
                         var claseSalvo = "celdaSalvo";
                         idCeldaSalvo = data.salvoes[keyTuJugador].turn[n];
                     }
                 }
             }
 
-            contenidoRejillaBarcos1 += '<td id="'+idCeldaBarco+'" class="'+claseBarco+'">'+idCeldaBarco+'</td>';
-            contenidoRejillaSalvos1 += '<td id="'+idCeldaBarco+'" class="'+claseSalvo+'">'+idCeldaBarco+'</td>';
+            contenidoRejillaBarcos1 += '<td id="' + idCeldaBarco + '" class="' + claseBarco + '">' + idCeldaBarco + '</td>';
+            contenidoRejillaSalvos1 += '<td id="' + idCeldaBarco + '" class="' + claseSalvo + '">' + idCeldaBarco + '</td>';
         }
         contenidoRejillaBarcos1 += '</tr>';
         contenidoRejillaSalvos1 += '</tr>';
@@ -90,23 +91,23 @@ function crearRejillaBarcos(data) {
 
     // ------------FIN PINTAR CELDAS BARCOS Y SALVOS---------
 
-    // console.log(contenidoRejillaSalvos1);
-
-    document.getElementById("gameID").innerHTML = contenidoRejillaBarcos1;
-    document.getElementById("gameID2").innerHTML = contenidoRejillaSalvos1;
+    document.getElementById("rejillaBarcosID").innerHTML = contenidoRejillaBarcos1;
+    document.getElementById("rejillaSalvosID").innerHTML = contenidoRejillaSalvos1;
+}
 
     // ---------------JUGADORES--------------
+    function crearJugadoresGV(data) {
 
-    var crearDatosJugadoresGamesView = "";
+        var datosJugadoresGamesView = "";
 
+        if (limpiarURL(document.location.search) == data.gameplayers[0].gamePlayerID) {
+            datosJugadoresGamesView = data.gameplayers[0].player.playerEmail + "(YOU) VS. " + data.gameplayers[1].player.playerEmail;
+        }
+        else {
+            datosJugadoresGamesView = data.gameplayers[1].player.playerEmail + "(YOU) VS. " + data.gameplayers[0].player.playerEmail;
+        }
 
-    if (limpiarURL(document.location.search) ==data.gameplayers[0].gamePlayerID){
-        crearDatosJugadoresGamesView = data.gameplayers[0].player.playerEmail+"(YOU) VS. "+data.gameplayers[1].player.playerEmail;
+        document.getElementById("jugadoresGamesviewID").innerHTML = datosJugadoresGamesView;
+
     }
-    else {
-        crearDatosJugadoresGamesView = data.gameplayers[1].player.playerEmail+"(YOU) VS. "+data.gameplayers[0].player.playerEmail;
-    }
 
-    document.getElementById("jugadoresGamesviewID").innerHTML = crearDatosJugadoresGamesView;
-
-}
