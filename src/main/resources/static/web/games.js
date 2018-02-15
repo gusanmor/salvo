37af897f718ba5af70b5a541f19291d9c30244ff
Gustavo <gustavosamo@gmail.com>
2018-02-15 13:36:31 +0100

    task 5, creando array objetos jugadores con puntuaciones, win, lost(3)

--- src/main/resources/static/web/games.js
+++ src/main/resources/static/web/games.js
@@ -1,63 +1,38 @@
-// console.log("prueba");
-
 $.getJSON("http://localhost:8080/api/games", function (data) {
-
-    	// console.log(data);
+    console.log(data);
     tablaLeaderBoard(data);
-
 });
 
 function tablaLeaderBoard(data) {
-
-    console.log(data);
-    // console.log(data[0].gamePlayers[1].score);
-
-// }
-//
 
     var contenidoLeaderBoard = "";
     var nombresJugadores = [];
 
 // CONSEGUIR TODOS LOS JUGADORES QUITANDO LOS REPETIDOS////
     for (var i=0; i<data.length; i++) {
-
         nombresJugadores.push(data[i].gamePlayers[0].player.playerEmail);
         nombresJugadores.push(data[i].gamePlayers[1].player.playerEmail);
-
     }
-    // console.log(nombresJugadores);
-
     var nomJugNoRepetidos = [];
     $.each(nombresJugadores, function(i, el){
         if($.inArray(el, nomJugNoRepetidos) === -1) nomJugNoRepetidos.push(el);
     });
-
-    console.log(nomJugNoRepetidos);
-
+    // console.log(nomJugNoRepetidos);
 
     arrayObjJugadores = [];
     for (var j=0; j<nomJugNoRepetidos.length; j++) {
 
-
-        // objetosJugadores.points =
-        // objetosJugadores.won = cogerWonJugador(data, nomJugNoRepetidos[j]);
-        // console.log(nomJugNoRepetidos[j]);
         arrayObjJugadores.push(cogerPuntosJugador(data, nomJugNoRepetidos[j]));
 
     }
     ordenarMembers(arrayObjJugadores);
     console.log(arrayObjJugadores);
-
     document.getElementById("tablaID").innerHTML = "<td>hola</td>";
-//
 }
-
-// cogerPuntosJugador("j.bauer@ctu.gov");
 
 function cogerPuntosJugador(data, nombreJugador){
 
     objetosJugadores = {};
-    // console.log("hola");
     objetosJugadores.name = nombreJugador;
     objetosJugadores.points = 0.0;
     objetosJugadores.win = 0;
@@ -69,7 +44,7 @@ function cogerPuntosJugador(data, nombreJugador){
 
     for (var k=0; k<data.length; k++) {
         for (var l=0; l<2; l++) {
-            console.log(nombreJugador);
+            // console.log(nombreJugador);
             if (data[k].gamePlayers[l].player.playerEmail == nombreJugador) {
                 if (data[k].gamePlayers[l].score != "null") {
                     // console.log("diferente");
